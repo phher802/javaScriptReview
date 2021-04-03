714b89883e5ea6825fa110ad51ffedd6aa3edda5
phher802 <pher024@gmail.com>
2021-04-02 23:05:20 -0500

    Add challenge 4 to multi-pointers-pattern file

--- concrete_problems/multi-pointers-pattern.js
+++ concrete_problems/multi-pointers-pattern.js
@@ -98,3 +98,42 @@ function averagePair(arr, num){
     }
     return false;
 }
+
+//--------------
+//challenge 4: 
+//write a function called isSubsequence which takes in two strings and checks whether the characters
+//in the first string form a subsequence of the charactes in the second string
+//in other words, the function should check whether the characters in the first string appear
+//somewhere in the second string w/o their order changing
+
+isSubsequence('hello', 'hello world'); //true
+isSubsequence('sing', 'sting');  //true
+isSubsequence('abc', 'abracadabra');  //true
+isSubsequence('abc', 'acb');  //false (order matters)
+
+//break it down:
+    //use multiple pointers; set a var for each array at 0 index
+    //convert each string into an array
+    //
+
+    function isSubsequence(str1, str2){
+        var i = 0;
+        var j= 0; 
+
+        if(!str1){
+            return true;
+        }
+
+        while(j < str2.length){
+
+            if(str2[j] === str1[i]){
+                i++;
+            }
+
+            if(i === str1.length){
+                return true;
+                j++;
+            }
+        }
+        return false;
+    }
