b610a70cd941904d53bd139dc344e0870984c4f5
phher802 <pher024@gmail.com>
2021-04-03 21:37:40 -0500

    Add challenge problem 2 to slidingWindow file

--- concrete_problems/slidingWindow.js
+++ concrete_problems/slidingWindow.js
@@ -29,3 +29,42 @@ function maxSubarraySum(arr, num){
 
     return maxSum;
 }
+
+//--------------------
+//challenge problem 2: maxSubarraySum
+//given an array of integers and a number, write a function called maxSubarraySum
+//which finds the max sum of a subarray with the length of the number passed to the function
+//subarrays must consist of consecutive elements from the original array
+
+maxSubarraySum2([100, 200, 300, 400], 2) //700
+maxSubarraySum2([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)  //39
+maxSubarraySum2([-3, 4, 0, -2, 6, -1], 2 ) //5
+maxSubarraySum2([2, 3], 3) //null
+
+//break it down:
+//write edge case length is less than num, return false
+//set a var called total to zero
+// loop thru array and add to total
+// set a new var called currentTotal equal to total 
+// new loop thru array.length and add to current total
+
+function maxSubarraySum2(arr, num){
+    if(arr.length < num){
+        return false;
+    }
+
+    let total = 0;
+
+    for (let i = 0; i < num; i++){
+        total += arr[i];
+    }
+
+    let currentTotal = total;
+
+    for (let i = num; i < arr.length; i++){
+        currentTotal += arr[i] - arr[i - num];
+        total = Math.max(total, currentTotal);
+    }
+
+    return total;
+}
