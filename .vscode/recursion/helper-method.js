68c99b5d3b1f8ab8e2ea9cfe1ef13792b9e45199
phher802 <pher024@gmail.com>
2021-04-04 23:06:38 -0500

    Add helper-method file to recursion folder

---
+++ .vscode/recursion/helper-method.js
@@ -0,0 +1,40 @@
+
+//recursion helper method
+//has two functions: outer and inside function that calls itself recursively
+//example one:
+
+function outer (input) {
+	
+	var outerScopedVariable = [];
+	
+	function helper (helperInput){
+		//modify the outterScopedVariable
+		helper(helperInput--);
+}
+
+	helper(input)
+
+	return outerScopedVariable;
+}
+
+
+//example 2:
+
+function collectOddValues(arr){
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length === 0){
+            return;
+        }
+
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+
+        helper(helperInput.slice(1));
+    }
+    
+    helper (arr);
+    return result;
+}
