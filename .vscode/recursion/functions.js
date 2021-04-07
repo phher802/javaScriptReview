ff3b6a0e617d3831d90533534f5d06cde197540a
phher802 <pher024@gmail.com>
2021-04-06 22:28:44 -0500

    Add new function to function file in recursion folder

--- .vscode/recursion/functions.js
+++ .vscode/recursion/functions.js
@@ -27,3 +27,13 @@ function sumRange(num){
 //what is the different input 
     //sumRange(num - 1)
 //what is returned from the base case matters because it's added on to what the previous function call was wating for
+
+//----------
+//challenge 2:
+//write a function called power which accepts a base and an exponent; the function should return the power
+//of the base to the exponent; the function should mimic the functionality of Math.pow()
+
+function power(base, exponent){
+    if(exponent === 0) return 1;
+    return base * power(base, exponent - 1);
+}
