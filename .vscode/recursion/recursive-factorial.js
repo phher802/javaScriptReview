f6d7e518e87358f06c0cae4ac164d16053f34e88
phher802 <pher024@gmail.com>
2021-04-06 22:32:46 -0500

    Add challenge 1 function to recursive-factorial file in recursion folder

--- .vscode/recursion/recursive-factorial.js
+++ .vscode/recursion/recursive-factorial.js
@@ -14,3 +14,14 @@ function factorial(num){
     if(num === 1) return 1;
     return num * factorial(num -1);
 }
+
+//---
+//challenge 1: write a function factorial which acccepts a number and returns the factorial of that number
+//a factorial is the product of an integer and all the intergers below it
+//i.e. factorial (4!) is equal to 24, b/c 4*3*2*1 equals 24; factorial zero(0!) is always 1.
+
+function factorial(x){
+    if(x < 0) return 0;
+    if(x <=1) return 1;
+    return x * factorial(x-1);
+}
