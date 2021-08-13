932fa049bb189e2c1447f6d12a02b4e8517d6ca1
phher802 <pher024@gmail.com>
2021-08-12 22:42:48 -0500

    Add fizzbuzz.js to controlflow folder'

---
+++ Mosh/ControlFlow/fizzbuzz.js
@@ -0,0 +1,19 @@
+//divisible by 3 == Fizz
+//divisible by 5 == Buzz
+//divisible by both 3 & 5 == FizzBuzz
+//not divisible by 3 or 5 == input
+//not a number == "not a number";
+
+function fizzBuzz(input){
+    if(typeof input !== 'number') return 'Not a number';
+  
+    if((input % 3 === 0) && (input % 5 === 0)) return 'FizzBuzz';
+    
+    if(input % 3 === 0) return 'Fizz';
+    if(input % 5 === 0) return 'Buzz';
+  
+    return input;
+}
+
+let findFizzBuzz = fizzBuzz(15);
+console.log(findFizzBuzz);
