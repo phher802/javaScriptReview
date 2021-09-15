deb0075ec7f88ae5ffff5d360e66f9bfe0c4593f
phher802 <pher024@gmail.com>
2021-09-14 21:51:14 -0500

    Add SumOf.. js folder to ControlFlow folder

---
+++ Mosh/ControlFlow/SumOfMultiplesOf3And5.js
@@ -0,0 +1,25 @@
+//this funciton, passing in a limit, which will return the sum of all the multiples 
+//of 3 and 5 up to the limit from zero
+
+
+function sum(limit){
+ 
+var results = 0;
+
+  for(let i = 0; i <= limit; i++){
+  
+      if(i % 3 === 0){
+          console.log(i);
+          results += i;
+      }
+      if(i % 5 ===  0){
+          console.log(i);
+          results += i;
+      }
+      
+  }
+  return results;
+
+}
+
+console.log(sum(10));
