78453a83137b8afc5f7e5a0c02f690f537acdd65
phher802 <pher024@gmail.com>
2021-09-15 20:01:44 -0500

    Add factory function to basics.js file

--- Mosh/Objects/basics.js
+++ Mosh/Objects/basics.js
@@ -15,3 +15,19 @@ const circle = {
 };
 
 circle.draw();  //calling the circle draw method
+
+//Factory Functions -- a way to produce objects
+//this example also simplifies the circle object above
+function createCircle(radius){
+    return {
+        radius,
+        draw() {
+            console.log("draw");
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+const circle2 = createCircle(2);
+console.log(circle2);
